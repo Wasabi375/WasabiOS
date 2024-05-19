98c7bdb24aba704d43b2fe7c2278cfc6de2a5c5c
Burkhard Mittelbach <wasabi37a@googlemail.com>
2024-05-19 16:41:46 +0200

    Test: add t_assert_matches and into for test errors

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -7,7 +7,7 @@
 
 extern crate alloc;
 
-use core::error::Error;
+use core::{error::Error, fmt::Debug};
 
 pub use testing_derive::kernel_test;
 use thiserror::Error;
@@ -24,8 +24,8 @@ pub enum KernelTestError {
     Assert,
 }
 
-/// Extensio for the Result type useful for tests
-pub trait TestResultExt<T> {
+/// Extensio for the Result/Option type useful for tests
+pub trait TestUnwrapExt<T> {
     /// Same as `unwrap` but returns [KernelTestError]
     ///
     /// This should be called with the question mark operator:
@@ -38,7 +38,13 @@ pub trait TestResultExt<T> {
     fn texpect(self, message: &str) -> Result<T, KernelTestError>;
 }
 
-impl<T, E: Error> TestResultExt<T> for Result<T, E> {
+// TODO replace tfail! with custom error message
+//  and use core::intrinsics::caller_location.
+//  Otherwise we report the wrong code location.
+//  Does that work with panic=abort
+//  This probably also needs #[track_caller] and I might want
+//  to use #[inline]
+impl<T, E: Error> TestUnwrapExt<T> for Result<T, E> {
     fn tunwrap(self) -> Result<T, KernelTestError> {
         match self {
             Ok(v) => Ok(v),
@@ -51,6 +57,52 @@ impl<T, E: Error> TestResultExt<T> for Result<T, E> {
             Ok(v) => Ok(v),
             Err(_err) => tfail!("{}", message),
         }
+    }
+}
+
+impl<T> TestUnwrapExt<T> for Option<T> {
+    fn tunwrap(self) -> Result<T, KernelTestError> {
+        match self {
+            Some(v) => Ok(v),
+            None => tfail!("Expected Some found None"),
+        }
+    }
+
+    fn texpect(self, message: &str) -> Result<T, KernelTestError> {
+        match self {
+            Some(v) => Ok(v),
+            None => tfail!("{}", message),
+        }
+    }
+}
+
+/// A wrapper that implements [core::fmt::Display] via the underlying types [core::fmt::Debug]
+pub struct DisplayDebug<T: Debug>(pub T);
+
+impl<T: Debug> core::fmt::Debug for DisplayDebug<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<T: Debug> core::fmt::Display for DisplayDebug<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        <T as Debug>::fmt(&self.0, f)
+    }
+}
+
+impl<T: Debug> core::error::Error for DisplayDebug<T> {}
+
+/// A extension trait for `Result<T, Debug>` which mapps the debug error value
+/// in [DisplayDebug].
+pub trait DebugErrResultExt<T, E: Debug> {
+    /// wraps the Err variant in [DisplayDebug]
+    fn map_err_debug_display(self) -> Result<T, DisplayDebug<E>>;
+}
+
+impl<T, E: Debug> DebugErrResultExt<T, E> for Result<T, E> {
+    fn map_err_debug_display(self) -> Result<T, DisplayDebug<E>> {
+        self.map_err(|e| DisplayDebug(e))
     }
 }
 
@@ -184,6 +236,44 @@ macro_rules! t_assert_ne {
                     );
                     return Err($crate::KernelTestError::Assert);
                 }
+            }
+        }
+    };
+}
+
+/// Same as `assert_matches!` but returns a [KernelTestError] instead of `panic`
+#[macro_export]
+macro_rules! t_assert_matches {
+    ($left:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? $(,)?) => {
+        match $left {
+            $( $pattern )|+ $( if $guard )? => {}
+            ref expr_val => {
+                log::error!(
+                    target: "TEST",
+                    "at {}\nAssertion expr matchs pattern failed\nexpr: {} = {:?}\npattern: {}",
+                    $crate::location_str!(),
+                    $crate::__macro_internals::stringify!($left),
+                    expr_val,
+                    $crate::__macro_internals::stringify!($($pattern)|+ $(if $guard)?),
+                );
+                return Err($crate::KernelTestError::Assert);
+            }
+        }
+    };
+    ($left:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )?, $($arg:tt)+) => {
+        match $left {
+            $( $pattern )|+ $( if $guard )? => {}
+            ref expr_val => {
+                log::error!(
+                    target: "TEST",
+                    "at {}\nAssertion expr matchs pattern failed: {}\nexpr: {} = {:?}\npattern: {}",
+                    $crate::location_str!(),
+                    $crate::__macro_internals::format_args!($($arg)+),
+                    $crate::__macro_internals::stringify!($left),
+                    expr_val,
+                    $crate::__macro_internals::stringify!($($pattern)|+ $(if $guard)?),
+                );
+                return Err($crate::KernelTestError::Assert);
             }
         }
     };
