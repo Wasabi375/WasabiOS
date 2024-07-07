479437523be3ecd31436ac4280bde5f99a935979
Burkhard Mittelbach <wasabi37a@googlemail.com>
2024-07-07 13:32:35 +0200

    Shared: Impl Debug for InvalidValue (error for U8Enum)

--- shared/src/primitive_enum.rs
+++ shared/src/primitive_enum.rs
@@ -1,6 +1,7 @@
 //! utilites for the primitive enum proc-macro
 
 /// Error type used to denote that a given value is invalid for the operation
+#[derive(Debug)]
 pub struct InvalidValue<T> {
     /// the invalid value
     pub value: T,
