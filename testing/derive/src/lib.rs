f75e924c693ad0d5e9e2cb924d5877a789c2075d
Burkhard Mittelbach <wasabi37a@googlemail.com>
2024-09-26 16:14:39 +0200

    Tests: Document kernel_test proc-macro

--- testing/derive/src/lib.rs
+++ testing/derive/src/lib.rs
@@ -7,7 +7,41 @@ use syn::parse_macro_input;
 mod args;
 mod declaration;
 
-// TODO provide proper docs
+/// Marks a function as a kernel test
+///
+/// This replaces rusts `test` proc macro.
+///
+/// # Arguments:
+///
+/// * name: <test name>
+///     Rename the test
+/// * expected_exit: <exit_value>
+///     The test is expected to exit with the given [TestExitState]
+/// * focus|x|f
+///     If any test is marked as focused, then only focused tests are executed.
+///     This can be used to override the ignore flag.
+/// * ignore|i
+///     The test is ignored
+/// * multiprocessor|mp
+///     The test is using multiple cores, an can take a [DataBarrier] input parameter
+///
+/// # Example
+/// ```
+/// #[kernel_test]
+/// fn test_fn() -> Result<(), KernelTestError> {}
+///
+/// #[kernel_test(name: test_2)]
+/// fn test_with_custom_name() -> Result<(), KernelTestError> {}
+///
+/// #[kernel_test(i f)]
+/// fn ignored_and_focused_test() -> Result<(), KernelTestError> {}
+///
+/// #[kernel_test(mp)]
+/// fn mp_test() -> Result<(), KernelTestError> {}
+///
+/// #[kernel_test(mp)]
+/// fn mp_test_with_barrier(db: &DataBarrier<Box<dyn Any + Send>>) -> Result<(), KernelTestError> {}
+//// ```
 #[proc_macro_attribute]
 pub fn kernel_test(attribute: TokenStream, item: TokenStream) -> TokenStream {
     let args = parse_macro_input!(attribute as Args);
