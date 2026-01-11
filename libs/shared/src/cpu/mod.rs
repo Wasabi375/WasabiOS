ecc8254e8954ecac6b4f2376ae2da41daf327e2f
Burkhard Mittelbach <wasabi37a@googlemail.com>
2026-01-11 12:48:32 +0100

    Add read_instruction_pointer! macro

--- libs/shared/src/cpu/mod.rs
+++ libs/shared/src/cpu/mod.rs
@@ -2,3 +2,24 @@
 //! that are not dependent on the kernel
 
 pub mod time;
+
+/// Reads the instruction pointer.
+///
+/// Returns a [x86_64::VirtAddr] with the instruction pointer when this macro is called.
+#[macro_export]
+macro_rules! read_instruction_pointer {
+    () => {
+        {
+            let ip: u64;
+            // Safety just reading rip is save
+            unsafe {
+                asm!(
+                    "lea {}, [rip]",
+                    out (reg) ip,
+                    options(nostack)
+                );
+            }
+            x86_64::VirtAddr::new(ip)
+        }
+    };
+}
